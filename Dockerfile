38993de7639d05d4d60b639103a3926972646dc3
Felipe Mendonça <fysp11@gmail.com>
2021-10-28 19:46:47 +0200

    added some changes (#1)

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,14 @@ FROM python:3
 
 WORKDIR /usr/src/app
 
+ENV FLASK_APP=source/app.py
+ENV FLASK_RUN_HOST=0.0.0.0
+
+ENV FLASK_ENV=development 
+
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD [ "python", "./run.py" ]
+CMD ["flask", "run"]
--- source/app.py
+++ source/app.py
@@ -1,28 +1,21 @@
 import time
 import os
-from flask import Flask, render_template, flash, redirect, request, url_for
+from flask import Flask, flash, request
 from flask_sqlalchemy import SQLAlchemy
-
-
-DBUSER = os.getenv("DBUSER")
-DBPASS = os.getenv("DBPASS")
-DBHOST = os.getenv("DBHOST")
-DBPORT = os.getenv("DBPORT")
-DBNAME = os.getenv("DBNAME")
 
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///albums.sqlite3'
-app.config['SQLALCHEMY_DATABASE_URI'] = \
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+a =\
     'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{db}'.format(
-        user=DBUSER,
-        passwd=DBPASS,
-        host=DBHOST,
-        port=DBPORT,
-        db=DBNAME)
+        user=os.getenv("DBUSER"),
+        passwd=os.getenv("DBPASS"),
+        host=os.getenv("DBHOST"),
+        port=os.getenv("DBPORT"),
+        db=os.getenv("DBNAME"))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'foobarbaz'
-
 
 db = SQLAlchemy(app)
 
@@ -51,15 +44,19 @@ def database_initialization_sequence():
     db.session.rollback()
     db.session.commit()
 
+@app.route('/')
+def home():
+    return 'Flask with docker!'
 
-@app.route('/albums', method=['GET'])
+@app.route('/albums', methods=['GET'])
 def get_albums():
+    app.logger.info('im here')
     return{
         "data": albums.query.all(),
         "message":"success"
     }
 
-@app.route('/albums', method=['POST'])
+@app.route('/albums', methods=['POST'])
 def new_album():
     album = albums(
             request.form['name'],
@@ -82,4 +79,4 @@ if __name__ == '__main__':
         else:
             dbstatus = True
     database_initialization_sequence()
-    app.run(debug=True, host='0.0.0.0',port='8000')
+    app.run(debug=True, host='0.0.0.0',port=8000)
